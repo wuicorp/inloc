3f2d53711674a5b0538b5b546e698c04b6c0f69c
Albert Viscasillas <aviscasillas@gmail.com>
2015-07-01 09:27:45 +0200

    Add factory_girl and database_cleaner

---
+++ spec/support/doorkeeper_factories.rb
@@ -0,0 +1,16 @@
+FactoryGirl.define do
+  factory :access_token, class: Doorkeeper::AccessToken do
+    sequence(:resource_owner_id) { |n| n }
+    application
+    expires_in 2.hours
+
+    factory :clientless_access_token do
+      application nil
+    end
+  end
+
+  factory :application, class: Doorkeeper::Application do
+    sequence(:name) { |n| "Application #{n}" }
+    redirect_uri 'https://app.com/callback'
+  end
+end
